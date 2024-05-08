cf5615d036a9a788043137c8ba3b4ca93b8e6bf4
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-05-08 09:41:16 +0530

    commit

---
+++ react-portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import styles from "./App.module.css";
+
+function App() {
+
+
+  return (
+    <div className={styles.App}>
+    
+    Hello World
+    
+    </div>
+  
+  )
+}
+
+export default App
