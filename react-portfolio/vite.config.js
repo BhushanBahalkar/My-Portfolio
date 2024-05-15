905ed08b9aa8f13fd8fff779c4b1f775f09f6a97
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-05-15 15:33:47 +0530

    update vite.config.js

--- react-portfolio/vite.config.js
+++ react-portfolio/vite.config.js
@@ -5,11 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   
-  // css:{
-  //   modules:{
-  //     localsConvention: "camlCase" ,
-  //   },
-  // },
+
   css:{
     modules:{
       localsConvention:"camelCase",
